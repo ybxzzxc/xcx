cd9d0a2c0e9af603eac3b6dc2ca748166e4005cf
ybxzzxc <45807268+ybxzzxc@users.noreply.github.com>
2023-07-30 13:22:18 +0800

    new

---
+++ Study-main/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <tabbar v-show="$route.meta.isshowtabbar"></tabbar>
+    <!--   一级路由-->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import tabbar from "@/components/tabbar";
+
+export default {
+  name: 'App',
+  components: {
+   tabbar,
+  },
+  methods:{
+
+  },
+  data(){
+    return {active:0}
+  },
+  created() {
+
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+}
+</style>
